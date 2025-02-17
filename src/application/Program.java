1613ed16caf2356f7ef5e860819f6ee07f63c9d6
Blazz <143752142+Eduardo-Bruggmann@users.noreply.github.com>
2025-02-17 15:48:34 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,9 +11,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		ChessMatch chessMatch = new ChessMatch();
-		
+
 		while (true) {
 			UI.clearScreen();
 
@@ -21,11 +21,11 @@ public class Program {
 			System.out.println();
 			System.out.print("Source: ");
 			ChessPosition source = UI.readChessPosition(sc);
-			
+
 			System.out.println();
 			System.out.print("Target: ");
 			ChessPosition target = UI.readChessPosition(sc);
-			
+
 			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 		}
 	}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,22 +9,22 @@ import chess.pieces.Rook;
 public class ChessMatch {
 
 	private Board board;
-	
+
 	public ChessMatch() {
 		board = new Board(8, 8);
 		initialSetup();
 	}
-	
+
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-		for(int i = 0; i < board.getRows(); i++) {
-			for(int j = 0; j < board.getColumns(); j++) {
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
 				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
 		}
 		return mat;
 	}
-	
+
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
@@ -32,24 +32,27 @@ public class ChessMatch {
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
-	
+
 	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		return capturedPiece;
 	}
-	
+
 	private void validateSourcePosition(Position position) {
-		if(!board.thereIsAPiece(position)) {
+		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
+		}
 	}
-	
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
+
 	private void initialSetup() {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('b', 1, new Rook(board, Color.WHITE));
@@ -57,7 +60,7 @@ public class ChessMatch {
 		placeNewPiece('d', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
 		placeNewPiece('f', 1, new Rook(board, Color.WHITE));
-		
+
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('b', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
